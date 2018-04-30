51eaa0f686fe28577d0209d1b9315ddf25f094ec
Pablo Arranz Ropero <arranzropablo@gmail.com>
2018-04-30 20:06:59 +0200

    adsdad

--- tuenti6-java/src/Main.java
+++ tuenti6-java/src/Main.java
@@ -1,124 +1,73 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
 
     public static Scanner sc = new Scanner(System.in);
     public static int maxScore;
 
-    //terreno de mxn
-    //lava -> # (invalid)
-    //tierra -> .
-    //trampolin -> * (se mueve en L 4-2)
-    //se mueven en L(2-1) pero no puede caer en invalid ni fuera
-    //el caballero en S tiene qe rescatar a la princesa en P y llevarla a D
-    	
-    //Esta era la solución con backtracking pero es demasiado complejo y cuando hay más de 531 casillas le cuesta mucho
-    
-//    public static void maxScore(List<Nota> notasList, boolean[][] marcado, Coordinates<Integer, Integer> currentCell, int saltos, int nFilas, int nColumnas, Coordinates<Integer, Integer> objetivo) {
-//    		for (int i = 0; i < 8; i++) {
-//    			List<Boolean> marcadoList = new ArrayList<>();
-//    			for(boolean[] array : marcado) {
-//    				for(boolean elem : array) {
-//    					marcadoList.add(elem);
-//    				}
-//    			}
-//        		if (!marcadoList.contains(false)) {
-//        			return;
-//        		}
-//    			Coordinates<Integer, Integer> nextCell = daPaso(currentCell, i, mapa);
-//    			if(isValidCell(nextCell, nFilas, nColumnas, mapa) && !marcado[nextCell.getRow()][nextCell.getCol()]) {
-//    				saltos += 1;
-//    				marcado[nextCell.getRow()][nextCell.getCol()] = true;
-//    				if(nextCell.getRow() == objetivo.getRow() && nextCell.getCol() == objetivo.getCol()) {
-//    					//he llegado
-//    					if(saltos < minsaltos) {
-//    						minsaltos = saltos;
-//    					}
-//    				} 
-//    				//poda para que si ya lleva mas que el minimo no siga por ahí
-//    				else if(saltos < minsaltos) {
-//    					encontrarCamino(mapa, marcado, nextCell, saltos, nFilas, nColumnas, objetivo);
-//    				}
-//    				saltos -= 1;
-//    				marcado[nextCell.getRow()][nextCell.getCol()] = false;
-//    			}
-//    		}
-//    }
-     
-//  //Inicializamos marcas a false;
-//    void mochila(int valor[], int peso[], int m, int max, int k, int sumaPeso, bool marca[], int &maxValor) {
-//
-//    	int i = k; //int i=0;
-//    	while (i < n && !marca[i] && sumaPeso + peso[i] < m) {
-//    		sumaPeso += peso[i];
-//    		max += valor[i];
-//    		marca[i] = true;
-//    		if (max > maxValor || maxValor == -1)
-//    			maxValor = max;
-//    		mochila(valor, peso, m, max, k + 1, sumaPeso, marca, maxValor);
-//    		sumaPeso -= peso[i];
-//    		max -= valor[i];
-//    		marca[i] = false;
-//    	}
-//    }
-    
-	public static void maxScore(List<Nota> notasList, int score, int currentTime, Nota prevNota) {
+	public static void maxScore(List<Nota> notasList, int score, int currentTime, int prevTime) {
 		for(int i = 0; i < notasList.size(); i++) {
-			if(isValid(notasList.get(i), currentTime, prevNota)) {
-				//si se pulsa a la vez no se actualiza el tiempo
-				//mirar el timepo de salida de la anterior y el de esta y q no se actualice el tiempo
+			if(isValid(notasList.get(i), currentTime)) {
 				score += notasList.get(i).score;
-				//como puedo hacer sto?? le cuesta crear variables y se qeda un poco pillao..
-				int auxCurrentTime = currentTime;
-				if(prevNota == null || (notasList.get(i).initposition / notasList.get(i).speed) != (prevNota.initposition / prevNota.speed)) {
-					//si son notas qe NO se pulsan a la vez actualizamos current time
-					currentTime = (notasList.get(i).initposition / notasList.get(i).speed) + (notasList.get(i).length / notasList.get(i).speed);
-				}
+				currentTime = (notasList.get(i).initposition / notasList.get(i).speed) + (notasList.get(i).length / notasList.get(i).speed);
 				notasList.get(i).used = true;
 				if(score > maxScore) {
 					maxScore = score;
 				}
-				maxScore(notasList, score, currentTime, notasList.get(i));
+				maxScore(notasList, score, currentTime, currentTime);
 				//puedo sumarlo y pasarselo restando y luego restarselo
 				notasList.get(i).used = false;
 				score -= notasList.get(i).score;
-				if(prevNota == null || (notasList.get(i).initposition / notasList.get(i).speed) != (prevNota.initposition / prevNota.speed)) {
-					//si son notas qe NO se han pulsan a la vez actualizamos current time
-					if(prevNota == null) {
-						currentTime = 0;
-					} else {
-						currentTime = (prevNota.initposition / prevNota.speed) + (prevNota.length / prevNota.speed);
-					}
-				}
+				currentTime = prevTime;
 			}
 		}
 	}
 	
-	public static boolean isValid(Nota nota, int currentTime, Nota prevNota) {
-		//puedo pulsar dos notas a la vez (que empiecen y terminen estrictamente a la vez
+	public static boolean isValid(Nota nota, int currentTime) {
 		
-		return !nota.used && (currentTime < (nota.initposition / nota.speed) || ((nota.initposition / nota.speed) == (prevNota.initposition / prevNota.speed) && (nota.length / nota.speed) == (prevNota.length / prevNota.speed)));
+		return !nota.used && currentTime < nota.initposition / nota.speed;
 	}
     
+    //versión con pre-filtrado
+//	public static void maxScore(List<Nota> notasList, int score, int currentTime, final int prevTime) {
+//		notasList = notasList.stream().filter(nota -> {return !nota.used && prevTime < nota.initposition/nota.speed;} ).collect(Collectors.toList());
+//		for(int i = 0; i < notasList.size(); i++) {
+//			score += notasList.get(i).score;
+//			currentTime = (notasList.get(i).initposition / notasList.get(i).speed)
+//					+ (notasList.get(i).length / notasList.get(i).speed);
+//			notasList.get(i).used = true;
+//			if (score > maxScore) {
+//				maxScore = score;
+//			}
+//			maxScore(notasList, score, currentTime, currentTime);
+//			notasList.get(i).used = false;
+//			score -= notasList.get(i).score;
+//			currentTime = prevTime;
+//		}
+//	}
+    
     public static void main(String[] args) {
-					// i = k en el vuelta atras para q el primero sea uno diferente en cada intento y el for q recorra todo excepto él mismo
 		int nCasos = sc.nextInt();
 		for (int i = 0; i < nCasos; i++) {
 			int nNotas = sc.nextInt();
 			List<Nota> listNotas = new ArrayList<>();
 			for (int j = 0; j < nNotas; j++) {
-				listNotas.add(new Nota(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), false));
+				int X = sc.nextInt();
+				int L = sc.nextInt();
+				int S = sc.nextInt();
+				int P = sc.nextInt();
+				Optional<Nota> nota = listNotas.stream().filter(any -> any.initposition / any.speed == X / S && any.length / any.speed == L / S).findFirst();
+				if(nota.isPresent()) {
+					listNotas.get(listNotas.indexOf(nota.get())).score += P;
+				} else {
+					listNotas.add(new Nota(X, L, S, P, false));
+				}				
 			}
 			maxScore = 0;
-			maxScore(listNotas, 0, 0, null);
+			maxScore(listNotas, 0, 0, 0);
 			System.out.println("Case #" + String.valueOf(i + 1) + ": " + String.valueOf(maxScore));
 		}
 		
@@ -131,7 +80,7 @@ public class Main {
 	  public final int speed;
 	  public final int length;
 	  public final int initposition;
-	  public final int score;
+	  public int score;
 	  public boolean used;
 
 	  public Nota(int initposition, int length, int speed, int score, boolean used) {
